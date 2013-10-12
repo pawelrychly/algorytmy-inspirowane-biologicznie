192461e81086bbd7eac69bafeb592adcb5ff7028
Pawe³ Rych³y <pawelrychly@gmail.com>
2013-10-12 22:35:36 +0200

    Dodano funkcję wczytującą dane z pliku

--- atsp/src/atsp.cpp
+++ atsp/src/atsp.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 using namespace std;
 #include <time.h>
+#include <fstream>
+#include <sstream>
+
 
 
 double measure_time(double accuracy) {
@@ -28,10 +31,79 @@ double measure_time(double accuracy) {
 	return result;
 }
 
+int** read_data(string filename, int &dimension) {
+	dimension = -1;
+	int** matrix;
+	ifstream data;
+	string line = "";
+	int offset = 0;
+	filename = "../data-atsp/" + filename;
+	data.open(filename.c_str());
+
+	if (data.is_open()) {
+		while (!data.eof() && (dimension == -1)) {
+			getline(data, line);
+			if ((offset = line.find("DIMENSION:", 0)) != string::npos) {
+				istringstream iss;
+				iss.str(line.substr(offset + 10));
+				iss >> dimension;
+				matrix = new int*[dimension];
+				for (int i = 0; i < dimension; i++) {
+					matrix[i] = new int[dimension];
+				}
+			}
+		}
+
+		bool reading_matrix = false;
+		while (!data.eof() && (dimension > 0) && (!reading_matrix)) {
+			getline(data, line);
+			if ((offset = line.find("EDGE_WEIGHT_SECTION", 0)) != string::npos) {
+				reading_matrix = true;
+				break;
+			}
+		}
+		int x = 0;
+		int y = 0;
+		while (!data.eof() && (reading_matrix)) {
+			getline(data, line);
+			if ((offset = line.find("EOF", 0)) != string::npos) {
+				reading_matrix = false;
+				break;
+			}
+			std::stringstream  linestream(line);
+			int value;
+			// Read an integer at a time from the line
+			while(linestream >> value)
+			{
+				matrix[x][y] = value;
+				x = (x+1) % dimension;
+				if (x == 0) {
+					y++;
+				}
+			}
+		}
+
+	}
+
+	data.close();
+	if (matrix != NULL) {
+		return matrix;
+	}
+	return NULL;
+}
+
+
 int main() {
 	double result = measure_time(1);
-	cout << endl;
-	cout << "Wynik: ";
 	cout << result << " ms"<< endl; // prints !!!Hello World!!!
+	int dimension = 0;
+	int** mat = read_data("br17.atsp", dimension);
+	for (int i = 0; i < dimension; i++) {
+		for (int j = 0; j < dimension; j++) {
+			cout << mat[j][i] << " ";
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
