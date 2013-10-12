d696ada7c27ebcfe58d6f3f2b21d9688da694d89
Pawe³ Rych³y <pawelrychly@gmail.com>
2013-10-12 17:26:56 +0200

    Dodano funkcję mierzącą czas.

--- atsp/src/atsp.cpp
+++ atsp/src/atsp.cpp
@@ -8,8 +8,30 @@
 
 #include <iostream>
 using namespace std;
+#include <time.h>
+
+
+double measure_time(double accuracy) {
+	double prec = 1;
+	clock_t start, measure;
+	start = clock(); // bie¿¹cy czas systemowy w ms
+	int i = 0;
+	do {
+		//example function
+
+
+		measure = clock() - start;
+		i++;
+	} while((measure < prec/accuracy) || (i <= 10));
+	long m =(long)(measure);
+	double result = (double) m / i;
+	return result;
+}
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	double result = measure_time(1);
+	cout << endl;
+	cout << "Wynik: ";
+	cout << result << " ms"<< endl; // prints !!!Hello World!!!
 	return 0;
 }
