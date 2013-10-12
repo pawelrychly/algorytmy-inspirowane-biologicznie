cd077342b1b762ca1233dd18f2d937d532611afe
Pawe³ Rych³y <pawelrychly@gmail.com>
2013-10-12 13:53:14 +0200

    Init commit

---
+++ atsp/src/atsp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : atsp.cpp
+// Author      : Pawe³ Rych³y Dawid Wiœniewski
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
